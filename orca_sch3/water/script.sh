81739b916169f8e0c9da049a7e4e40da9b68e452
Since-Ufa <muranaufa@gmail.com>
2025-12-22 19:54:19 +0300

    m

---
+++ orca_sch3/water/script.sh
@@ -0,0 +1,28 @@
+#cd exci_BHandHLYP/
+#nohup /opt/orca6/orca uv-vis_BHandHLYP.inp &> out_uv-vis_BHandHLYP.out
+#cd ..
+
+
+cd exci_CAM-B3LYP
+nohup /opt/orca6/orca uv-vis_CAM-B3LYP.inp > out_uv-vis_CAM-B3LYP.out
+cd ..
+
+
+cd exci_M06-2X
+nohup /opt/orca6/orca uv-vis_M06-2X.inp > out_uv-vis_M06-2X.out
+cd ..
+
+
+cd exci_PBE0
+nohup /opt/orca6/orca uv-vis_PBE0.inp > out_uv-vis_PBE0.out
+cd ..
+
+
+cd exci_r2SCAN50
+nohup /opt/orca6/orca uv-vis_r2SCAN50.inp > out_uv-vis_r2SCAN50.out
+cd ..
+
+
+cd exci_wB97X
+nohup /opt/orca6/orca uv-vis_wB97X.inp > out_uv-vis_wB97X.out
+cd ..
